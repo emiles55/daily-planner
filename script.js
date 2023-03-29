9f191d83574a0786d1af3108d3ad264a37cc2452
Emily Miles <emilymiles193@gmail.com>
2023-03-29 19:20:08 -0400

    worked on javascript

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-var today = dayjs().format("dddd, MMMM D YYYY");
+setDate=document.getElementById("currentDay");
+var today = dayjs().format("dddd, MMMM D, YYYY");
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  setDate.innerText=today;
+  const btns = document.querySelectorAll('button[id^=saveBtn]')
+
+  const buttons = document.querySelectorAll('.saveBtn')
+  buttons.forEach(function(currentBtn){
+    currentBtn.addEventListener('click', saveData)
+  })
+  function saveData(){
+
+  }
+  
 
    // "17-6-2022"
   // TODO: Add a listener for click events on the save button. This code should
