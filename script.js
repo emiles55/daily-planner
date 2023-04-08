e91a89f2de76966a5b66624e747f845e3b0e995e
Emily Miles <emilymiles193@gmail.com>
2023-04-08 11:56:10 -0400

    worked on saving and loading text

--- script.js
+++ script.js
@@ -58,4 +58,14 @@ console.log(blockHour);
   //
   // TODO: Add code to display the current date in the header of the page.
   changeColor();
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
 });
